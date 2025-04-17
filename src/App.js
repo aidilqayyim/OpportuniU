ff1541bbb1da101db5e519bed8cfdd037903ea9f
irfan <irfandaniel2504@gmail.com>
2025-04-18 01:14:10 +0800

    irfan

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Cards from "./components/Cards";
 import Footer from "./components/Footer";
 import SignIn from "./components/Signin";
 import Joblistings from "./components/Joblistings";
-import SignUp from "./components/SignUp";
+
 
 function App() {
   return (
@@ -32,6 +32,16 @@ function App() {
           }
         />
       </Routes>
+      <Route 
+        path="/joblistings"
+        element={
+          <>
+            <Navbar />
+            <Joblistings />
+            <Footer />
+          </>
+        }
+      />
     </Router>
   );
 }
