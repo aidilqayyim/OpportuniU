109a36d5e53068b49f8219fba7becbfdeb29afb3
Aidil Qayyim <muhdaidilqayyim@gmail.com>
2025-06-11 21:13:31 +0800

    Updates

---
+++ static/js/198.612dac65.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkopportuniu=self.webpackChunkopportuniu||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.612dac65.chunk.js.map
